7be49cf502015dc897e1d60a778068a7e0c86d89
Jenny-Reinhard <55107673+Jenny-Reinhard@users.noreply.github.com>
2019-11-03 08:02:32 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "browser": true,
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-console": 0,
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      0,
+      "error",
+      "windows"
+    ],
+    "global-require": [
+      0
+    ]
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
